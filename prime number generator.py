1b1d0bc5730d2c9f0d3cdd687fde21738e5387ff
DLS Student Temp <DLSSTUDENTTEMP@FED.CCLRC.AC.UK>
2017-07-04 15:10:01 +0100

    I correctly indendent my code so it conforms to PEP8

--- prime number generator.py
+++ prime number generator.py
@@ -2,13 +2,13 @@
 numberinrangelist = range(2,1000)
 
 for number in numberinrangelist:
-  isprime = True
-  divisors = range(2,number)
-  for divisor in divisors: 
-    if number%divisor == 0:
-      isprime = False
-  if isprime == True:
-    print number
+    isprime = True
+    divisors = range(2,number)
+    for divisor in divisors: 
+      if number%divisor == 0:
+        isprime = False
+    if isprime == True:
+      print number
       
       
 raw_input()
