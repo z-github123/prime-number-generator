63f6e5b51d7b9015e29a8509a6cf3eeea613c1b9
DLS Student Temp <DLSSTUDENTTEMP@FED.CCLRC.AC.UK>
2017-07-04 16:48:50 +0100

    I added indentations so my code conforms to PEP8

--- prime number generator.py
+++ prime number generator.py
@@ -1,10 +1,10 @@
 
 def isprime(number):
-  for divisor in range (2, number):
-    if number%divisor == 0:
-      return False
-  return True
+    for divisor in range (2, number):
+      if number%divisor == 0:
+        return False
+    return True
     
 for number in range(2,1000):
-  if isprime (number):
-    print number
+    if isprime (number):
+      print number
